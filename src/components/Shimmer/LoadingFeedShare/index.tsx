4d51c335845a7fb95c280d447a9966935f286694
Glauber Brack <glauber@brack.com.br>
2020-08-19 15:24:36 -0300

    LoadingTrendingPanel of RightComponent

--- src/components/Shimmer/LoadingFeedShare/index.tsx
+++ src/components/Shimmer/LoadingFeedShare/index.tsx
@@ -1,9 +1,19 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import Panel from '../../Panel';
+import Skeleton from '../../Skeleton';
+
+import { Container } from './styles';
 
 const LoadingFeedShare: React.FC = () => {
-  return <div />;
+  return (
+      <Container>
+          <Panel className="no-shadow">
+              <Skeleton className="row-skeleton"/>
+              <Skeleton className="row-skeleton"/>
+          </Panel>
+      </Container>
+  );
 }
 
 export default LoadingFeedShare;
