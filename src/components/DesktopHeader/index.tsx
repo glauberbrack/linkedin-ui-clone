9331d77f4713c84b1ecf1eed478e61dfd0053362
Glauber Brack <glauber@brack.com.br>
2020-08-19 10:45:46 -0300

    started desktopheader

---
+++ src/components/DesktopHeader/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import { Container, Wrapper, LinkedinIcon, SearchInput, HomeIcon, NotificationIcon, ProfileIcon, CaretDownIcon } from './styles';
+
+import profileImg from '../../assets/images/profile.jpg';
+
+const DesktopHeader: React.FC = () => {
+    return (
+        <Container>
+            <Wrapper>
+            <div className="left">
+                <LinkedinIcon />
+                <SearchInput placeholder="Search" />
+            </div>
+
+            <div className="rio">
+                <nav>
+                    <button className="active">
+                        <HomeIcon />
+                        <span>Home</span>
+                    </button>
+                    <button >
+                        <NotificationIcon />
+                        <span>Notifications</span>
+                    </button>
+                    <button >
+                        <ProfileIcon src={profileImg}  />
+                        <span>
+                            Me <CaretDownIcon />
+                        </span>
+                    </button>
+                </nav>
+            </div>
+            </Wrapper>
+        </Container>
+    );
+};
+
+export default DesktopHeader;
---
+++ src/components/DesktopHeader/styles.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const Container = styled.div``;
+
+export const Wrapper = styled.div``;
+
+export const LinkedinIcon = styled.div``;
+
+export const SearchInput = styled.div``;
+
+export const HomeIcon = styled.div``;
+
+export const NotificationIcon = styled.div``;
+
+export const ProfileIcon = styled.div``;
+
+export const CaretDownIcon = styled.div``;
