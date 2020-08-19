87843cb6d16d714f2c7d20727174493c105c5b97
Glauber Brack <glauber@brack.com.br>
2020-08-19 10:28:08 -0300

    edit profile component

--- src/components/LeftColumn/ProfilePanel/index.tsx
+++ src/components/LeftColumn/ProfilePanel/index.tsx
@@ -14,17 +14,17 @@ const ProfilePanel: React.FC = () => {
           alt="Avatar"
           className="profile-picture"
         />
-        <h1>Guilherme Rodz</h1>
-        <h2>Software Engineer @ Rocketseat</h2>
+        <h1>Glauber Brack</h1>
+        <h2>Software Developer at CBYK</h2>
 
         <div className="separator"></div>
 
         <div className="key-value">
-          <span className="key">Quem viu seu perfil</span>
+          <span className="key">Who viewed your profile</span>
           <span className="value">1.558</span>
         </div>
         <div className="key-value">
-          <span className="key">Viram sua publicação</span>
+          <span className="key">Views of your post</span>
           <span className="value">388</span>
         </div>
       </Container>
