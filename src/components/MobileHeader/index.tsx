6dad3194d9f5073356ad388a4ede02dffa5ac72d
Glauber Brack <glauber@brack.com.br>
2020-08-19 10:37:10 -0300

    import image for mobile header

--- src/components/MobileHeader/index.tsx
+++ src/components/MobileHeader/index.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 import { Container, ProfileCircle, SearchInput, MessageIcon } from './styles';
 
+import profileImg from '../../assets/images/profile.jpg';
+
 const MobileHeader: React.FC = () => {
     return (
         <Container>
-            <ProfileCircle src="https://media-exp1.licdn.com/dms/image/C4D03AQGUWtq9rh0weQ/profile-displayphoto-shrink_200_200/0?e=1603324800&v=beta&t=iz6emhu-LvwHORTtnNJaCXU6Go7peYPTk1Sy7byCpO8"/>
+            <ProfileCircle src={profileImg}/>
             <SearchInput placeholder="Pesquisar" />
             <MessageIcon />
         </Container>
